59abd91b932260ef83ad91d1c90f12aed0a957fa
okstring08 <okstring@zuminternet.com>
2022-07-09 15:04:12 +0900

    INIT: initial commit

---
+++ hometerview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hometerview
+//
+//  Created by Ok Hyeon Kim on 2022/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, hometerview")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hometerview/hometerviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  hometerviewApp.swift
+//  hometerview
+//
+//  Created by Ok Hyeon Kim on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct hometerviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
