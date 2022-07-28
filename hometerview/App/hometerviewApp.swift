e963702f6d644f87282c6994c128ff0d158ba879
okstring <milkoi7801@gmail.com>
2022-07-28 23:19:52 +0900

    add: User 구성, UserDefaults 읽기, 쓰기 메소드 구현

--- hometerview/App/hometerviewApp.swift
+++ hometerview/App/hometerviewApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct hometerviewApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            switch User.shared.status {
+                case .unAuthenticated:
+                    MainView()
+                case .authenticated:
+                    MainTabView()
+            }
         }
     }
 }
