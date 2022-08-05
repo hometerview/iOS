d21d229291fc2b621833760c923ac3780d088002
okstring <milkoi7801@gmail.com>
2022-08-06 00:28:22 +0900

    add: 홈화면 임시 구현

---
+++ hometerview/Sources/Presenter/UI/Home/View/HomeListCell.swift
@@ -0,0 +1,101 @@
+//
+//  HomeListCell.swift
+//  hometerview
+//
+//  Created by Ok Hyeon Kim on 2022/08/05.
+//
+
+import SwiftUI
+
+struct HomeListCell: View {
+    var body: some View {
+        LazyVStack(alignment: .leading, spacing: 16) {
+            Text("아크로텔 오피스텔")
+                .font(.pretendard(weight: .bold, size: 20))
+
+
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("icon_discovery")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.colorStyle(.gray400))
+                        .frame(width: 16, height: 16)
+                    Text("인천 남동구 구월남로 125")
+                        .font(.pretendard(size: 14))
+                }
+
+                HStack {
+                    Image("icon_home")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.colorStyle(.gray400))
+                        .frame(width: 16, height: 16)
+                    Text("오피스텔")
+                        .font(.pretendard(size: 14))
+                }
+            }
+
+            HStack(alignment: .center) {
+                makeStarred(count: 4)
+                HStack(spacing: 4) {
+                    Text("내 집이 최고")
+                    Image(systemName: "circle.fill")
+                        .font(.system(size: 2))
+                    Text("3년 전 거주")
+                    Image(systemName: "circle.fill")
+                        .font(.system(size: 2))
+                    Text("역삼역 통근")
+                }
+                .foregroundColor(.colorStyle(.gray600))
+                .font(.pretendard(size: 12))
+
+            }
+            .font(.pretendard(size: 12))
+
+
+            VStack(alignment: .leading, spacing: 6) {
+                Text("장점")
+                    .font(.pretendard(weight: .medium, size: 12))
+                .foregroundColor(.colorStyle(.blue300))
+
+                Text("역근처라 그런지 일단 인프라가 너무 좋아요!")
+                    .font(.pretendard(weight: .regular, size: 14))
+            }
+
+            VStack(alignment: .leading, spacing: 6) {
+                Text("단점")
+                    .font(.pretendard(weight: .medium, size: 12))
+                    .foregroundColor(.colorStyle(.red500))
+                Text("창문이 많아서 냉난방비 많이나오고 커튼달기도 쉽지 않음...")
+                    .font(.pretendard(weight: .regular, size: 14))
+            }
+
+            LikeButton(isLike: true)
+
+        }
+    }
+
+    @ViewBuilder func makeStarred(count: Int) -> some View {
+        HStack(spacing: 0) {
+            ForEach(0..<5, id: \.self) { starIndex in
+                Image("icon_star")
+                    .resizable()
+                    .renderingMode(.template)
+                    .if(starIndex < count, transform: { image in
+                        image
+                            .foregroundColor(.colorStyle(.blue300))
+                    })
+                    .frame(width: 16, height: 16)
+
+            }
+        }
+    }
+}
+
+struct HomeListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeListCell()
+            .previewLayout(.sizeThatFits)
+    }
+}
--- hometerview/Sources/Presenter/UI/Home/View/HomeView.swift
+++ hometerview/Sources/Presenter/UI/Home/View/HomeView.swift
@@ -7,18 +7,82 @@
 
 import SwiftUI
 
+import Introspect
+
 struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
 
     var body: some View {
-        Button {
-            print("Hello World!")
-        } label: {
-            Text("Hello World!")
-                .font(.pretendard(size: 16))
+        NavigationView {
+            List {
+                header
 
+                Section {
+                    ForEach(0..<2, id: \..self) { _ in
+                        HomeListCell()
+                    }
+                } header: {
+                    stickyHeader
+                }
+                .listRowBackground(Color.white)
+
+
+            }
+            .listStyle(.plain)
+            .ignoresSafeArea()
+            .introspectTableView { tableView in
+                tableView.separatorInset = .zero
+                tableView.separatorColor = .clear
+                tableView.contentInset = .zero
+            }
+
+            .navigationBarHidden(true)
+            .navigationBarTitleDisplayMode(.inline)
         }
+    }
 
+    var header: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Spacer()
+
+                Text("삼성전자")
+                    .font(.pretendard(weight: .bold, size: 24))
+                    .padding(.bottom, 8)
+
+                Text("통근러에게 추천하는 동네")
+                    .font(.pretendard(weight: .medium, size: 24))
+                    .padding(.bottom, 8)
+
+                Text("동탄역 직장인들이 많이 사는 동네의 리뷰입니다")
+                    .font(.pretendard(size: 14))
+                    .padding(.bottom, 32)
+            }
+            .foregroundColor(.white)
+            .frame(height: 201)
+        }
+        .listRowBackground(Color.colorStyle(.blue300))
+    }
+
+    var stickyHeader: some View {
+        HStack {
+            Text("강남구")
+                .font(.pretendard(size: 14))
+                .padding(.leading)
+
+            Text("서초구")
+                .font(.pretendard(size: 14))
+                .padding(.leading)
+
+            Text("중구")
+                .font(.pretendard(size: 14))
+                .padding(.leading)
+
+            Spacer()
+        }
+        .frame(height: 50)
+        .background(Color.gray)
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
     }
 }
 
