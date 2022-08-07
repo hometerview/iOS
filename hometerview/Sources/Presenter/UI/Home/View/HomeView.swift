d17d6b5b3bc7970f9d7a5033148762d408c95f49
okstring <milkoi7801@gmail.com>
2022-08-07 15:09:14 +0900

    add: Home - RankHeader 추가

--- hometerview/Sources/Presenter/UI/Home/View/HomeView.swift
+++ hometerview/Sources/Presenter/UI/Home/View/HomeView.swift
@@ -12,17 +12,20 @@ import Introspect
 struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
     @State private var stickyHeaderSelectedIndex = 0
+    @State private var selectedRankIndex = 0
     @Namespace private var bottomLine
 
     var body: some View {
         NavigationView {
             ZStack {
-                Color.colorStyle(.blueGrey200)
+                Color.colorStyle(.blueGrey100)
                     .ignoresSafeArea()
                 
                 ScrollView {
                     LazyVStack(alignment: .leading) {
                         header
+
+                        rankHeader
 
                         ForEach(0..<10, id: \..self) { _ in
                             LazyVStack {
@@ -66,6 +69,27 @@ struct HomeView: View {
                 .padding(.bottom, 32)
         }
         .padding(.vertical, 32)
+        .padding(.horizontal)
+    }
+
+    var rankHeader: some View {
+        HStack {
+            ForEach(0..<3, id: \.self) { index in
+                Button {
+                    selectedRankIndex = index
+                } label: {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(maxWidth: .infinity, minHeight: 80)
+                        .foregroundColor(index == selectedRankIndex ? .colorStyle(.blue300) : .colorStyle(.blueGrey200))
+                        .overlay(
+                            Text("갱냄구")
+                                .font(.pretendard(size: 16, weight: .semibold))
+                                .foregroundColor(index == selectedRankIndex ? .white : .colorStyle(.gray600))
+
+                        )
+                }
+            }
+        }
         .padding(.horizontal)
     }
 
