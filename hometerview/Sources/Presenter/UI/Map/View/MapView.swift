80dcff50f69033f6dbfbfbc0bfd48e4398245c75
okstring <milkoi7801@gmail.com>
2022-08-07 16:11:48 +0900

    add: MapView 가이드 구현

--- hometerview/Sources/Presenter/UI/Map/View/MapView.swift
+++ hometerview/Sources/Presenter/UI/Map/View/MapView.swift
@@ -31,7 +31,29 @@ struct KakaoMapView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: MTMapView, context: Context) {
+        addPinIcon(mapView: uiView)
+        moveWithZoomLevel(mapView: uiView)
 
+    }
+
+    func moveWithZoomLevel(mapView: MTMapView) {
+        mapView.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.500723072486, longitude: 127.03680544372)), animated: true)
+        mapView.setZoomLevel(4, animated: true)
+    }
+
+    func addPinIcon(mapView: MTMapView) {
+        let item = MTMapPOIItem()
+        item.itemName = "역삼역"
+        item.markerType = .customImage
+        item.customImageName = "icon_pin.png"
+        item.customCalloutBalloonView = UIView()
+
+        item.markerSelectedType = .none
+
+        item.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo.init(latitude: 37.500723072486, longitude: 127.03680544372))
+        item.customImageAnchorPointOffset = MTMapImageOffset(offsetX: 30, offsetY: 0)
+
+        mapView.addPOIItems([item])
     }
 
     class Coordinator: NSObject, MTMapViewDelegate {
@@ -39,6 +61,19 @@ struct KakaoMapView: UIViewRepresentable {
 
         init(_ parent: KakaoMapView) {
             self.parent = parent
+        }
+
+        func mapView(_ mapView: MTMapView!, finishedMapMoveAnimation mapCenterPoint: MTMapPoint!) {
+            // 지도 이동이 끝났을 때
+        }
+
+        func mapView(_ mapView: MTMapView!, zoomLevelChangedTo zoomLevel: MTMapZoomLevel) {
+            // 확대/축소 레벨이 변경됐을 때
+        }
+
+        func mapView(_ mapView: MTMapView!, selectedPOIItem poiItem: MTMapPOIItem!) -> Bool {
+            // 아이템 선택 시
+            return true
         }
     }
 
