d080a0323c01f22a6f6a87c9769665eca34999e0
Okhyeon Kim <62657991+okstring@users.noreply.github.com>
2022-10-04 23:03:25 +0900

    add: 별점 소수점 표현, foldering 진행 (#20)
    * modify: 일관성있게 폴더링 진행
    
    * modify: 별점 소수점 표현 구현
    
    * modify: 별점 - CGFloat 타입 통일

--- hometerview/Sources/Presenter/UI/Base/View/TotalStarred.swift
+++ hometerview/Sources/Presenter/UI/Base/View/TotalStarred.swift
@@ -8,26 +8,50 @@
 import SwiftUI
 
 struct TotalStarred: View {
-    let count: Int
+    struct ClipShape: Shape {
+        let width: CGFloat
+
+        func path(in rect: CGRect) -> Path {
+            Path(CGRect(x: rect.minX, y: rect.minY, width: width, height: rect.height))
+        }
+    }
+
+    let count: Float
     let width: CGFloat
 
     var body: some View {
         HStack(spacing: 0) {
-            ForEach(0..<5, id: \.self) { starIndex in
+            ForEach(0..<5, id: \.self) { _ in
                 Image("icon_star")
                     .resizable()
                     .renderingMode(.template)
-                    .foregroundColor(starIndex < count ? .colorStyle(.blue300) : .colorStyle(.gray200))
                     .frame(width: width, height: width)
-
+                    .foregroundColor(.colorStyle(.gray200))
+                    .aspectRatio(contentMode: .fill)
             }
-        }
+        }.overlay(
+            GeometryReader { reader in
+                HStack(spacing: 0) {
+                    ForEach(0..<5, id: \.self) { _ in
+                        Image("icon_star")
+                            .resizable()
+                            .renderingMode(.template)
+                            .frame(width: width, height: width)
+                            .foregroundColor(.colorStyle(.blue300))
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                .clipShape(
+                    ClipShape(width: (reader.size.width / CGFloat(5)) * CGFloat(count))
+                )
+            }
+        )
     }
 }
 
 struct TotalStarred_Previews: PreviewProvider {
     static var previews: some View {
-        TotalStarred(count: 4, width: 16)
+        TotalStarred(count: 4.421, width: 16)
             .previewLayout(.sizeThatFits)
     }
 }
