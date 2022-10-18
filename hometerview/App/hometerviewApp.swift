578d612eca7b0b80d13628e1945814830c9c3b41
okstring <milkoi7801@gmail.com>
2022-10-18 12:55:23 +0900

    modify: kakao 로그인 핸들링

--- hometerview/App/hometerviewApp.swift
+++ hometerview/App/hometerviewApp.swift
@@ -7,20 +7,49 @@
 
 import SwiftUI
 
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
+
 @main
 struct hometerviewApp: App {
+    @UIApplicationDelegateAdaptor var dleegate: AppDelegate
     @StateObject private var user = User.shared
 
     var body: some Scene {
         WindowGroup {
-            switch user.status {
-                case .unAuthenticated:
-                    MainView()
-                case .authenticated:
-                    MainTabView()
-                case .guest:
-                    MainTabView()
+            Group {
+                switch user.status {
+                    case .unAuthenticated:
+                        MainView()
+                            .onOpenURL { url in
+                                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                                    _ = AuthController.handleOpenUrl(url: url)
+                                }
+                            }
+                    case .authenticated:
+                        MainTabView()
+                    case .guest:
+                        MainTabView()
+                }
             }
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+
+        KakaoSDK.initSDK(appKey: "f811c290471ec82c6e65a3c0c68a3bbc")
+
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+
+        return false
     }
 }
